09c38b3f911223e603ed2ab36f875856068b4d94
sbastianpatzelt <sebastian@patze.lt>
2011-05-03 03:01:24 +0200

    Added OwnImplementation fpr Fizzbuzz

---
+++ FizzBuzzKata/FizzBuzzKata/FizzBuzzerSpa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using FizzBuzz.Objects;
+
+namespace FizzBuzzKata
+{
+    public class FizzBuzzerSpa : IFizzBuzzer
+    {
+        public string Get(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> Get(IEnumerable<int> i)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
